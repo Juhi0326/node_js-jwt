98f17b37cb94f9ba8c3a8ba24ff08de96ec17217
István Juhász <istvan.juhasz@telcotrend.hu>
2021-01-25 10:57:51 +0100

    use .env file for enviroment variables

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-
 const app = express();
+require('dotenv').config();
+
 
 // middleware
 app.use(express.static('public'));
+
+console.log(process.env.dbUser);
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI = `mongodb+srv://${process.env.dbUser}:${process.env.dbPass}@cluster0.hnfsi.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => {
+  app.listen(3000);
+  console.log('db connected');
+})
+
   .catch((err) => console.log(err));
 
 // routes
