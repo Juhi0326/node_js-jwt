cb6e5e0e25deb6afa78d9fc9164474d4a6cfdc6e
István Juhász <istvan.juhasz@telcotrend.hu>
2021-01-28 14:52:27 +0100

    set error messages for validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,24 @@
 const User = require('../models/User');
 
+//handle errors:
+handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = {email: '', password: ''};
+
+  //duplicate email error code
+  if (err.code === 11000) {
+    errors.email = 'this email is already registrated';
+    return errors;
+  }
+
+  //validatinon errors
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({properties}) => {
+      errors[properties.path] = properties.message;
+    })
+  }
+  return errors;
+}
 
 // controller actions
 module.exports.signup_get = (req, res) => {
@@ -16,8 +35,8 @@ module.exports.signup_post = async (req, res) => {
    const user = await User.create({email, password})
    res.status(201).json(user);
   } catch (err) {
-    console.log(err);
-    res.status(400).send('error, user is not created')
+    const errors = handleErrors(err);
+    res.status(400).json( {errors} );
   }
 }
 
