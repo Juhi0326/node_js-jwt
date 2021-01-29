26c9d3be0aaae791ea581f5ad7650350520ca04f
István Juhász <istvan.juhasz@telcotrend.hu>
2021-01-29 08:37:12 +0100

    hash password before doc saved

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 const {isEmail} = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -15,6 +16,22 @@ const userSchema = new mongoose.Schema({
     minlength: [6, 'Minimum password lenght is 6 characters']
   },
 });
+
+//fire a function after doc saved to db
+
+userSchema.post('save', function(doc, next) {
+  console.log('ez a user-ből jön: ', doc)
+  next();
+})
+
+//hash password before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+})
+
+
 
 const User = mongoose.model('user', userSchema);
 
