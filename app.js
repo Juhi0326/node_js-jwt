44774a2bd6959a2a04288b56bce4a8c65799f9ce
István Juhász <istvan.juhasz@telcotrend.hu>
2021-01-29 12:50:04 +0100

    create signup and login forms and handle cookies

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 require('dotenv').config();
 
@@ -8,6 +10,7 @@ require('dotenv').config();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -26,3 +29,22 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+//cookies
+app.get('/set-cookies', (req, res) => {
+  
+  res.cookie('newUser', false);
+  res.cookie('employee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true },)
+
+  res.send('you got the cookies!')
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies);
+
+});
