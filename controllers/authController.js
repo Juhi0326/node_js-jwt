d703e108385b6559bbb8aec735c1adf612785273
István Juhász <istvan.juhasz@telcotrend.hu>
2021-01-29 14:48:57 +0100

    show error messages and relocate in case of succes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,47 +1,57 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 //handle errors:
 handleErrors = (err) => {
   console.log(err.message, err.code);
-  let errors = {email: '', password: ''};
+  let errors = { email: "", password: "" };
 
   //duplicate email error code
   if (err.code === 11000) {
-    errors.email = 'this email is already registrated';
+    errors.email = "this email is already registrated";
     return errors;
   }
 
   //validatinon errors
-  if (err.message.includes('user validation failed')) {
-    Object.values(err.errors).forEach(({properties}) => {
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
       errors[properties.path] = properties.message;
-    })
+    });
   }
   return errors;
-}
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+const craeteToken = (id) => {
+  return jwt.sign({ id }, "juhi secret", {
+    expiresIn: maxAge,
+  });
+};
 
 // controller actions
 module.exports.signup_get = (req, res) => {
-  res.render('signup');
-}
+  res.render("signup");
+};
 
 module.exports.login_get = (req, res) => {
-  res.render('login');
-}
+  res.render("login");
+};
 
 module.exports.signup_post = async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   try {
-   const user = await User.create({email, password})
-   res.status(201).json(user);
+    const user = await User.create({ email, password });
+    const token = craeteToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({user: user._id});
   } catch (err) {
     const errors = handleErrors(err);
-    res.status(400).json( {errors} );
+    res.status(400).json({ errors });
   }
-}
+};
 
 module.exports.login_post = async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   console.log(email, password);
-  res.send('user login');
-}
+  res.send("user login");
+};
